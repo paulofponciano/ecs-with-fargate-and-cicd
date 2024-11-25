5547fffacf8aa8725b46fa8d1810c6cf902e0d93
Paulo Ponciano <96355974+paulofponciano@users.noreply.github.com>
2024-11-25 09:17:17 -0300

    output

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "s3_bucket_arn" {
   description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
-  value       = try(module.s3_bucket.this[0].arn, "")
+  value       = module.s3_bucket.s3_bucket_arn
 }
 
 output "route53_record_fqdn" {
